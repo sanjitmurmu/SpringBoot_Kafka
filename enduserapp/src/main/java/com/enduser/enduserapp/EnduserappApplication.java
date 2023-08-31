4fccbb2068c21df1e5bcb64d98e77c53c39d61a4
murmu <murmu@homeProject>
2023-09-01 00:42:01 +0530

    SpringBoot with Kafka Service:Consumer

---
+++ enduserapp/src/main/java/com/enduser/enduserapp/EnduserappApplication.java
@@ -0,0 +1,13 @@
+package com.enduser.enduserapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnduserappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnduserappApplication.class, args);
+	}
+
+}
---
+++ enduserapp/src/test/java/com/enduser/enduserapp/EnduserappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enduser.enduserapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnduserappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
